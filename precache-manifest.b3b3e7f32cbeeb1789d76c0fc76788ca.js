9e5eb691d923be145aae1df64fe330b1e6b12c24
doru87 <32599645+doru87@users.noreply.github.com>
2020-05-04 11:55:35 +0300

    Updates

---
+++ precache-manifest.b3b3e7f32cbeeb1789d76c0fc76788ca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8667fa1456425d94d5823dd3999dee",
+    "url": "/moviedb/index.html"
+  },
+  {
+    "revision": "3b12814671d534980066",
+    "url": "/moviedb/static/css/main.0f3fa36e.chunk.css"
+  },
+  {
+    "revision": "b81adf45c7455f1cfd60",
+    "url": "/moviedb/static/js/2.59ccc10b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/moviedb/static/js/2.59ccc10b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b12814671d534980066",
+    "url": "/moviedb/static/js/main.19662aa8.chunk.js"
+  },
+  {
+    "revision": "70db7b48535e2664294d",
+    "url": "/moviedb/static/js/runtime-main.7d8cc4ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/moviedb/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "78b8b9853183e29bd82d98aafc60340c",
+    "url": "/moviedb/static/media/play.78b8b985.svg"
+  },
+  {
+    "revision": "c045f6f17772b638f398e84276500c3e",
+    "url": "/moviedb/static/media/signs.c045f6f1.svg"
+  },
+  {
+    "revision": "79acc15ff6b79d0e396ec280fea60b50",
+    "url": "/moviedb/static/media/technology.79acc15f.svg"
+  },
+  {
+    "revision": "1b3dbd685dee2a5c06d410aa2c77928f",
+    "url": "/moviedb/static/media/tripod.1b3dbd68.svg"
+  }
+]);
